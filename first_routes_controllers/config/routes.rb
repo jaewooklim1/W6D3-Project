2df9ebca6b28a35bf1c25c2f7d449a94be132beb
Daniel <danielwu116@gmail.com>
2021-03-31 13:59:22 -0400

    Add routes

--- first_routes_controllers/config/routes.rb
+++ first_routes_controllers/config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # resources :users
+
+  get '/users', to: 'users#index'
+  post '/users', to: 'users#create'#, as: 'users'
+  
+  get '/users/new', to: 'users#new', as: 'new_user'
+
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+
+  get '/users/:id', to: 'users#show', as: 'user'
+  patch '/users', to: 'users#update'#, as: 'user'
+  put '/users/:id', to: 'users#update'#, as: 'user'
+  delete '/users/:id', to: 'users#destroy'#, as: 'user'
+  
 end
